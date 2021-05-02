90cb6bd9887b094416e17c1a758dc50bdea937d8
cruelscript <cruelscript@gmail.com>
2021-05-02 19:15:49 +0300

    Initial commit.

---
+++ main.cpp
@@ -0,0 +1,35 @@
+﻿#include "parser.hpp"
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+void printHelp()
+{
+	cout << "Usage: cmilan input_file" << endl;
+}
+
+int main(int argc, char** argv)
+{
+	if (argc < 2)
+	{
+		printHelp();
+		return EXIT_FAILURE;
+	}
+
+	ifstream input;
+	input.open(argv[1]);
+
+	if (input)
+	{
+		Parser p(argv[1], input);
+		p.parse();
+		return EXIT_SUCCESS;
+	}
+	else
+	{
+		cerr << "File '" << argv[1] << "' not found" << endl;
+		return EXIT_FAILURE;
+	}
+}
+
